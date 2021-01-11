dabf3c1efd95c91fb63837f561d08dcbccb66adf
sammy <exgin3538@yahoo.com>
2021-01-10 22:29:00 -0500

    comments

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 /* Do Not Fetch an API Route from getStaticProps() or getStaticPaths() 
-because getStaticProps() and getStaticPaths() runs only on the server-side. It will never be run on the client-side. It won’t even be included in the JS bundle for the browser. 
+because getStaticProps() and getStaticPaths() runs only on the server-side. It
+will never be run on the client-side. It won’t even be included in the JS bundle
+for the browser. 
+
+Saving incoming data to your database
+Securely communicating with a third-party API
+Previewing draft content from your CMS 
 */
 export default function handler(req, res) {
   /*
