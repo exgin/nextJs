85b075bdef2a444fdbe2a891f7e4722c0311e0c0
sammy <exgin3538@yahoo.com>
2021-01-10 22:27:51 -0500

    finish tut

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+/* Do Not Fetch an API Route from getStaticProps() or getStaticPaths() 
+because getStaticProps() and getStaticPaths() runs only on the server-side. It will never be run on the client-side. It won’t even be included in the JS bundle for the browser. 
+*/
+export default function handler(req, res) {
+  /*
+  - req is an instance of http.IncomingMessage, plus some pre-built middlewares
+  you can see here.
+
+  - res is an instance of http.ServerResponse, plus some helper functions you can see here.
+  */
+  res.status(200).json({ text: 'Hello' });
 }
